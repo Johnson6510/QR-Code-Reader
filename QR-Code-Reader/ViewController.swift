2b789ec818b4b50d51978fe313610a5d54737396
Johnson <johnson6510@gmail.com>
2017-12-29 23:10:38 +0800

    initial version

---
+++ QR-Code-Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR-Code-Reader
+//
+//  Created by 黃健偉 on 2017/12/29.
+//  Copyright © 2017年 黃健偉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
